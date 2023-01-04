3a0cdd6e6c4b39d2ba3cc1350813ae47e9bf5aec
mikolajb21 <miko3412@gmail.com>
2023-01-04 18:26:24 +0100

    first communication with cert

--- server.py
+++ server.py
@@ -1,18 +1,12 @@
-import ssl, socket
+import socket
+import ssl
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain('/path/to/certchain.pem', '/path/to/private.key')
+context.load_cert_chain('cert.pem', 'key.key')
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
-    sock.bind(('127.0.0.1', 8443))
+    sock.bind(('127.0.0.1', 2137))
+    print("slucham")
     sock.listen(5)
     with context.wrap_socket(sock, server_side=True) as ssock:
         conn, addr = ssock.accept()
-
-
-def test():
-    pass
-
-
-if __name__ == '__main__':
-    test()
