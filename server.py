9b96f2a98fb4d5bfc5cca508207f244521924038
mikolajb21 <miko3412@gmail.com>
2023-01-05 15:09:04 +0100

    Now working with initial exchange

--- server.py
+++ server.py
@@ -10,3 +10,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
     sock.listen(5)
     with context.wrap_socket(sock, server_side=True) as ssock:
         conn, addr = ssock.accept()
+        mess = conn.recv(1024)
+        print(mess[0])
--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import socket
 import ssl
 
-hostname='127.0.0.1'
+host='127.0.0.1'
+hostname='Miko'
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 context.load_verify_locations('cert.pem')
 
-with socket.create_connection((hostname, 2137)) as sock:
+with socket.create_connection((host, 2137)) as sock:
     with context.wrap_socket(sock, server_hostname=hostname) as ssock:
         print(ssock.version())
+        ssock.send(bytes("DUPA", "utf-8"))
