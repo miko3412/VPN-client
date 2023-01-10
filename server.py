0c7ac9f73009a0be1efcb4584ec604ed14dc73b0
Mikolaj Blinowski <mikolaj.blinowski@gmail.com>
2023-01-10 17:24:38 +0100

    Working server and testing script

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import ssl
+import ipaddress
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 context.load_cert_chain('cert.pem', 'key.key')
@@ -7,8 +8,35 @@ context.load_cert_chain('cert.pem', 'key.key')
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
     sock.bind(('127.0.0.1', 2137))
     print("slucham")
-    sock.listen(5)
+    sock.listen()
     with context.wrap_socket(sock, server_side=True) as ssock:
         conn, addr = ssock.accept()
+        #while True:
         mess = conn.recv(1024)
-        print(mess[0])
+        ipVersion=format(mess[0],'08b')
+        ipVersion=int(ipVersion[:4], 2)
+        print(ipVersion)
+        if ipVersion==4:
+            ipHeader=20
+            destinationIP = mess[16:20]
+            destinationIP= '.'.join(f'{c}' for c in destinationIP)
+        elif ipVersion==6:
+            ipHeader=40
+            destinationIP = mess[24:40]
+            destinationIP= ipaddress.IPv6Address(destinationIP)
+            print(str(destinationIP))
+        #else:
+        #    break
+        tcpHeader=format(mess[(ipHeader+12)], '08b')
+        tcpHeader=int(tcpHeader[:4], 2)*4
+        tcpPort=mess[(ipHeader+2):(ipHeader+4)]
+        tcpPort=int.from_bytes(tcpPort, "big")
+        print(tcpHeader)
+        print(tcpPort)
+        print(mess[(ipHeader+tcpHeader):])
+        with socket.create_connection((str(destinationIP), tcpPort)) as sock2:
+            sock2.send(mess[52:])
+            resp = sock2.recv(1024)
+            #print(resp)
+            conn.send(resp)
+    sock.close()
--- test.py
+++ test.py
@@ -9,4 +9,8 @@ context.load_verify_locations('cert.pem')
 with socket.create_connection((host, 2137)) as sock:
     with context.wrap_socket(sock, server_hostname=hostname) as ssock:
         print(ssock.version())
-        ssock.send(bytes("DUPA", "utf-8"))
+        a = bytearray.fromhex('60032918006a06402a02a3114335e800380cd88752e7b8ce2a001450401b080e000000000000200ede160050bcf08cc66c6d6669801801fbbb8b00000101080a8d036aef4fcf7bf8474554202f20485454502f312e310d0a486f73743a20676f6f676c652e636f6d0d0a557365722d4167656e743a206375726c2f372e36382e300d0a4163636570743a202a2f2a0d0a0d0a')
+        print(a)
+        ssock.send(a)
+        response = ssock.recv(1024)
+        print(response)
