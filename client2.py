1dc868d9833f542a2823d5831c69a390ec67fa96
Mikolaj Blinowski <mikolaj.blinowski@gmail.com>
2023-01-15 13:39:36 +0100

    Working on http requests, but with infinite loop

--- client2.py
+++ client2.py
@@ -1,5 +1,6 @@
 import socket, ssl
 
+
 host='127.0.0.1'
 hostname='Miko'
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
@@ -8,25 +9,30 @@ context.load_verify_locations('cert.pem')
 with socket.create_connection((host, 2137)) as sock:
     with context.wrap_socket(sock, server_hostname=hostname) as ssock:
         print(ssock.version())
-        #while True:
         s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
         s.bind(('enp9s0', 0))
         while True:
-        # receive a package
-            packet = s.recvfrom(65565)
-            if len(packet)>52:
+            try:
+                packet = s.recvfrom(65565)
+                packet= packet[0][14:]
                 ipVersion=format(packet[0],'08b')
                 ipVersion=int(ipVersion[:4], 2)
                 if ipVersion==4:
                     ipHeader=20
                 elif ipVersion==6:
                     ipHeader=40
+                else: continue
                 tcpHeader=format(packet[(ipHeader+12)], '08b')
                 tcpHeader=int(tcpHeader[:4], 2)*4
                 tcpPort=packet[(ipHeader+2):(ipHeader+4)]
                 tcpPort=int.from_bytes(tcpPort, "big")
-                if tcpPort == 80:
+                print(tcpPort)
+                if tcpPort == 80 and len(packet) > 52 and len(packet[(ipHeader+tcpHeader):]) > 5:
                     print(packet)
                     ssock.send(packet)
                     response = ssock.recv(1024)
-                    print(response)
+                    print("-----------------------------To jest odpowiedz -------------------------------")
+                    print(response.hex())
+                    print("\n\n")
+            except IndexError:
+                continue
